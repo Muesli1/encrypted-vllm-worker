543bcd10364479685e17c539cfb69ef49b8b4175
Muesli1 <mc01@neumann.to>
2024-01-07 04:18:00 +0100

    fix: register docker args & env

--- builder/setup.sh
+++ builder/setup.sh
@@ -4,6 +4,7 @@ set -e # Stop script on error
 apt-get update && apt-get upgrade -y # Update System
 
 # Download model via huggingface_hub snapshot_download
+echo Using model: \'$VLLM_MODEL_NAME\'
 python3.11 -c "from huggingface_hub import snapshot_download; snapshot_download(repo_id='$VLLM_MODEL_NAME')"
 
 # Clean up, remove unnecessary packages and help reduce image size
