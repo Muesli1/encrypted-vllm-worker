6a2a65df6ced98c867280487af467a7fe1efe803
Muesli1 <mc01@neumann.to>
2024-01-11 18:09:21 +0100

    feat: async streaming

--- src/handler.py
+++ src/handler.py
@@ -1,5 +1,6 @@
 import os
 import json
+from typing import AsyncGenerator
 
 import runpod
 
@@ -16,43 +17,64 @@ try:
 
     encryption_key = os.getenv("ENCRYPTION_KEY")
     if encryption_key is None:
-        encryption_key = "defaultkey"
+        encryption_handler = None
+    else:
+        encryption_handler = EncryptionHandler(encryption_key)
 
-    encryption_handler = EncryptionHandler(encryption_key)
-
-    from vllm import LLM, SamplingParams
+    from vllm import AsyncLLMEngine, SamplingParams
+    from vllm.utils import random_uuid
 
     llm_parameters = os.getenv("VLLM_ENGINE_PARAMETERS")
     if llm_parameters is None:
         llm_parameters = "{}"
 
     model_name = os.getenv("VLLM_MODEL_NAME")
-    llm = LLM(model=model_name, **json.loads(llm_parameters))
+    llm = AsyncLLMEngine(model=model_name, **json.loads(llm_parameters))
 except Exception as init_e:
     # Show to client when prompting
     model_init_error = init_e
 
 
-def handler(job):
+async def handler(job) -> AsyncGenerator[any, None]:
     print("Processing with version", VERSION, "...")
     try:
         if model_init_error is not None:
-            return {'error': str(model_init_error), 'init': True}
+            yield {'error': str(model_init_error), 'init': True}
+            return
 
         job_input = job['input']
-
-        prompt = encryption_handler.decrypt(job_input.get('prompt', False))
-        sampling_params = job_input.get('parameters', {})
+        prompt = job_input.get('prompt', False)
 
         if not prompt:
-            return {'error': 'Missing "prompt" key in input!'}
+            yield {'error': 'Missing "prompt" key in input!'}
+            return
 
-        outputs = llm.generate(prompt, SamplingParams(**sampling_params))
+        if encryption_handler is not None:
+            prompt = encryption_handler.decrypt(prompt)
+
+        request_id = random_uuid()
+        sampling_params = job_input.get('parameters', {})
+        generator = llm.generate(prompt, SamplingParams(**sampling_params), request_id)
+
+        if job_input.get("streaming", False):
+            async for async_request_output in generator:
+                yield async_request_output
+            return
+
+        final_output = None
+        async for request_output in generator:
+            final_output = request_output
 
         output_filter = job_input.get('output_filter', [])
-        return encryption_handler.encrypt(json.dumps(deep_filter(outputs, output_filter)))
+        filtered_output = deep_filter(final_output, output_filter)
+
+        if encryption_handler is not None:
+            yield encryption_handler.encrypt(json.dumps(filtered_output))
+        else:
+            yield filtered_output
+
     except Exception as e:
-        return {'error': str(e)}
+        yield {'error': str(e)}
 
 
-runpod.serverless.start({"handler": handler})
+runpod.serverless.start({"handler": handler, "return_aggregate_stream": True})
